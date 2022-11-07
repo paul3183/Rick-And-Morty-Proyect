d13fe3e96fc7e13bc3555639b2868caad964ce5e
Paul Ruiz <ruizp3183@gmail.com>
2022-11-06 20:14:03 -0500

    paul

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import Locations from './components/Locations'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Locations/>
+      <footer>
+        <img className="logo" src="https://o.remove.bg/downloads/9864614c-be37-44ce-99b6-503ce13e6bad/images-removebg-preview.png" alt=""/>
+        <h3>DEVELOPERS</h3>
+        <p>Paul Ruiz</p>
+        <p>Miguel Martinez</p>
+        <p>Robert </p>
+      </footer>
+    </div>
+  )
+}
+
+
+export default App
